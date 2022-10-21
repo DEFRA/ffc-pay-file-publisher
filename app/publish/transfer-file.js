b8b8900441d3a7974bb2e7e1adc784aa3933204f
John Watson <johnwatson484@hotmail.com>
2022-10-21 18:02:55 +0100

    Removed redundant link

--- app/publish/transfer-file.js
+++ app/publish/transfer-file.js
@@ -1,9 +1,7 @@
-const { shareConnectionString } = require('../config/storage')
 const getFile = require('./get-file')
 const storage = require('../storage')
 
 const transferFile = async (filename, ledger) => {
-  storage.connect(shareConnectionString)
   const { blob, content } = await getFile(filename)
   await storage.writeFile(filename, ledger, content)
   await storage.archiveFile(filename, blob)
