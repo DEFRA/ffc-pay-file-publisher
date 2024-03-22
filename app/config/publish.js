9e0390b439ed88c5bda6dc0e5842a22963b0e346
Sam Plackett <samuel.plackett@eviden.com>
2024-03-22 11:42:29 +0000

    add feature toggle

--- app/config/publish.js
+++ app/config/publish.js
@@ -3,13 +3,15 @@ const Joi = require('joi')
 const schema = Joi.object({
   totalRetries: Joi.boolean().default(5),
   retryInterval: Joi.number().default(1000),
-  useEvents: Joi.boolean().default(true)
+  useEvents: Joi.boolean().default(true),
+  enabled: Joi.boolean().default(true)
 })
 
 const config = {
   totalRetries: process.env.TOTAL_RETRIES,
   retryInterval: process.env.RETRY_INTERVAL,
-  useEvents: process.env.USE_EVENTS
+  useEvents: process.env.USE_EVENTS,
+  enabled: process.env.FILE_PUBLISH_ENABLED
 }
 
 const result = schema.validate(config, {
